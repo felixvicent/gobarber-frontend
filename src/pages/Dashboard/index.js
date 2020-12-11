a40810a3d36267dd87ba66806fc1799e953081e7
Felix Vicente <felixvicent1306@gmail.com>
2020-12-11 14:39:18 -0300

    Styling dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-import api from '~/services/api';
+// import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 function Dashboard() {
-  api.get('appointments');
-  return <hi>Dashboad</hi>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>20 de Dezembro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
 
 export default Dashboard;
